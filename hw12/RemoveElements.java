0b0a6f6388a339acae9b71e00054df3e5f6a2b1b
jwc218 <jwc218@lehigh.edu>
2016-04-19 19:00:31 +0000

    updated RemoveElements

--- hw12/RemoveElements.java
+++ hw12/RemoveElements.java
@@ -5,15 +5,6 @@
 //    11:10-12 lab
 import java.util.Scanner;
 import java.util.Random;
-// random input
-public static int [] randomInput(int num){
-Random rand = new Random();
-int [] array;
-array= new int[10];
-for(int i=0;i<10;i++){
- array[i]=rand.nextInt[10];
-}
-}
 public class RemoveElements{
   public static void main(String [] arg){
 	Scanner scan=new Scanner(System.in);
@@ -47,7 +38,88 @@ int index,target;
   	answer=scan.next();
 	}while(answer.equals("Y") || answer.equals("y"));
   }
- 
+  //randomInput
+public static int [] randomInput(){
+	// intiate random generator
+	Random rand = new Random();
+	// declare array length
+int [] array= new int[10];
+// assign random vals between 0 and 9
+for(int i=0;i<array.length;i++){
+ array[i]= rand.nextInt(10);
+}
+return array;
+}// end of randomInput method
+// delete
+public static int [] delete(int array[], int position){
+	// shrink size of array
+	int [] modifiedArray= new int[array.length-1];
+	for(int i=0; i<array.length-1;i++){
+		// shift numbers up after position
+		if(i>=position){
+			modifiedArray[i]= array[i+1];
+		}
+		// numbers before that position remain the same
+		else{
+			modifiedArray[i]= array[i];
+		}
+	}
+	return modifiedArray;
+}// end of delete method
+// remove
+public static int [] remove(int array[], int match){
+	int count=0;
+	for(int i=0; i<array.length;i++){
+		if (array[i]==match){
+			count++;
+		}
+	}
+	int var;
+	System.out.println(count);
+	int counter=0;
+	int [] modifiedArray= new int[10-count];
+	for(int k=0;k<10-count;k++){
+		System.out.print(k+"   "+ counter+"  ");
+	    if(array[k]==match && counter<count){
+	    	counter++;
+	    	if(k>0 && array[k]==array[k-1]){
+	    		counter--;
+	    	}
+	    	else if(array[k]==array[k+1]){
+	          counter++;
+	            if(array[k]==array[k+2]){
+	        	counter++;
+	        }
+	    	}
+	          while(counter<count && array[k+counter]==match){
+	       counter--;
+	        }
+	        while(counter>count){
+	        	counter--;
+	        }
+	    	modifiedArray[k]=array[k+counter];
+	    	}
+	       else{
+	       	if(counter>0){
+	            while (counter<count && array[k+counter]==match){
+	        counter++;
+	        }
+	        while(counter>count){
+	        counter--;
+	        }
+	    	modifiedArray[k]=array[k+counter];
+	         }
+	         else{
+	         modifiedArray[k]= array[k];	
+	         }
+	       }
+	       if(k==9-count && array[9]==match){
+	       	modifiedArray[k]=array[8];
+	       }
+       System.out.println(counter);
+	}
+return modifiedArray;
+}// end of remove program
   public static String listArray(int num[]){
 	String out="{";
 	for(int j=0;j<num.length;j++){
